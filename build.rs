dfaceade8fefe2d5eb6c8b74fd2600bb2f63b321
Mingun <alexander_sergey@mail.ru>
2016-12-18 03:02:02 +0500

    Доработан скрипт сборки для Windows - там библитека находится в другом месте

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 fn main() {
   let oracle_home = std::env::var("ORACLE_HOME").expect("Environment variable ORACLE_HOME not set");
-  println!("cargo:rustc-link-search={}/lib", oracle_home);
+  if cfg!(windows) {
+  	println!("cargo:rustc-link-search={}/oci/lib/msvc", oracle_home);
+  } else {
+  	println!("cargo:rustc-link-search={}/lib", oracle_home);
+  }
 }
