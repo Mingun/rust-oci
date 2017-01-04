899fb95bab00672cb2cd72df49c91f69394fc480
Mingun <alexander_sergey@mail.ru>
2017-01-04 15:59:26 +0500

    Добавлены скрипты для подготовки тестовых данных для тестов

---
+++ .travis/oracle/cleanup.sql
@@ -0,0 +1,40 @@
+-- Удаляем пользователя для примеров в документации, если он есть
+declare
+  does_not_exist   exception;
+  pragma EXCEPTION_INIT (does_not_exist, -1918);
+begin
+  execute immediate 'drop user username';
+exception
+  when does_not_exist then null;
+end;
+/
+---------------------------------------------------------------------------------------------------
+-- Вспомогательная функция для чистки базы. Не нужна при непрерывной интеграции, но полезна при разработке,
+-- когда база не откатывается каждый раз
+create or replace procedure delete_table(table_name in varchar2)
+as
+begin
+  declare
+    does_not_exist   exception;
+    pragma EXCEPTION_INIT (does_not_exist, -942);
+  begin
+    execute immediate 'drop table '||table_name||' purge';
+  exception
+    when does_not_exist then null;
+  end;
+end delete_table;
+/
+---------------------------------------------------------------------------------------------------
+-- Удаляем таблицы с тестовыми данными, если они есть. Не нужно при непрерывной интеграции, но полезно при разработке,
+-- когда база не откатывается каждый раз
+begin
+  delete_table('type_text');
+  delete_table('type_number');
+  delete_table('type_time');
+  delete_table('type_bin');
+  delete_table('type_lob');
+  delete_table('type_specific');
+end;
+/
+drop procedure delete_table;
+/
