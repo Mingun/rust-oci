5c05f2bdfd964ea3cb2d889fac8c84ba51f0235a
Mingun <Alexander_Sergey@mail.ru>
2017-01-06 17:51:37 +0500

    Подключаемся как SysDba это должно починить их

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,13 +1,14 @@
 
 use oci::{Environment, Connection};
 use oci::params::{ConnectParams, Credentials};
+use oci::types::AuthMode;
 
 pub fn connect<'e>(env: &'e Environment) -> Connection<'e> {
   let params = ConnectParams {
     dblink: "".into(),
     attach_mode: Default::default(),
     credentials: Credentials::Ext,
-    auth_mode: Default::default(),
+    auth_mode: AuthMode::SysDba,
   };
   env.connect(params).expect("Can't connect to ORACLE database")
 }
