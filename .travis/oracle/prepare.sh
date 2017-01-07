853264b7d9779fa17db7e07ab8a016e5d8c149e3
Mingun <Alexander_Sergey@mail.ru>
2017-01-07 23:36:50 +0500

    Исправлено ожидание ввода от sqlplus

--- .travis/oracle/prepare.sh
+++ .travis/oracle/prepare.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
-"$ORACLE_HOME/bin/sqlplus" -L -S / @.travis/oracle/cleanup.sql
+exit | "$ORACLE_HOME/bin/sqlplus" -L -S / @.travis/oracle/cleanup.sql
 "$ORACLE_HOME/bin/sqlplus" -L -S / <<SQL
 create or replace directory test_bfiles as '`pwd`/tests/resources';
 SQL
-"$ORACLE_HOME/bin/sqlplus" -L -S / @.travis/oracle/prepare.sql
-"$ORACLE_HOME/bin/sqlplus" -L -S / as sysdba @.travis/oracle/prepare-doc-examples.sql
+exit | "$ORACLE_HOME/bin/sqlplus" -L -S / @.travis/oracle/prepare.sql
+exit | "$ORACLE_HOME/bin/sqlplus" -L -S / as sysdba @.travis/oracle/prepare-doc-examples.sql
